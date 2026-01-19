3b8db3a846bbdf79e4c23171e5e4d61e49260669
YiguiDing <2449695354@qq.com>
2026-01-19 10:31:50 +0800

    update: 03.build pipeline dynamically

--- 03.build pipeline dynamically/main.c
+++ 03.build pipeline dynamically/main.c
@@ -1,6 +1,7 @@
 #include <gst/gst.h>
 
 /* Structure to contain all our information, so we can pass it to callbacks */
+// 用结构体包含所有元素为结构体，以便传递给回调函数。
 typedef struct _CustomData
 {
     GstElement *pipeline;
@@ -30,16 +31,22 @@ int main(int argc, char *argv[])
     gst_init(&argc, &argv);
 
     /* Create the elements */
+    /* 创建元素 */
+    // uri解码元素
     data.source = gst_element_factory_make("uridecodebin", "source");
-
-    data.videoconvert = gst_element_factory_make("videoconvert", "videoconvert");
-    data.videosink = gst_element_factory_make("autovideosink", "videosink");
-
+    // 视频处理相关元素
+    data.videoconvert = gst_element_factory_make("videoconvert", "videoconvert"); /* 视频格式转换 */
+    data.videosink = gst_element_factory_make("autovideosink", "videosink");      /* 视频播放 */
+    // 音频处理相关元素
+    /* audioconvert 转换格式以匹配前后音频编解码器的能力差异（不同平台下） */
     data.audioconvert = gst_element_factory_make("audioconvert", "audioconvert");
+    /* audioresample 重新采样以匹配前后音频编解码器的不同采样率 */
     data.audioresample = gst_element_factory_make("audioresample", "resample");
+    /* autoaudiosink 把音频流渲染到声卡上 */
     data.audiosink = gst_element_factory_make("autoaudiosink", "audiosink");
 
     /* Create the empty pipeline */
+    /* 创建空管道 */
     data.pipeline = gst_pipeline_new("test-pipeline");
     if (
         !data.pipeline                                                  // pileline
@@ -54,13 +61,25 @@ int main(int argc, char *argv[])
 
     /* Build the pipeline. Note that we are NOT linking the source at this
      * point. We will do it later. */
+    /**
+     * 构建管道。
+     * 将所有元素统一添加到管道管理
+     */
     gst_bin_add_many(
         GST_BIN(data.pipeline),                                // pileline
         data.source,                                           // source
         data.videoconvert, data.videosink,                     // video
         data.audioconvert, data.audioresample, data.audiosink, // audio
         NULL);
-
+    /**
+     * 连接元素
+     * source
+     *  - pad:src_0 -> videoconvert -> videosink
+     *  - pad:src_1 -> audioconvert -> audioresample -> audiosink
+     *
+     * 由于源元素uridecodebin的source_pad是在当收到一些数据后才会自动动态创建，
+     * 所以这里无法连接，只能后续等待pad-added信号在回调函数中连接。
+     */
     if (
         !gst_element_link_many(data.videoconvert, data.videosink, NULL)                        // video
         || !gst_element_link_many(data.audioconvert, data.audioresample, data.audiosink, NULL) // audio
@@ -75,6 +94,12 @@ int main(int argc, char *argv[])
     g_object_set(data.source, "uri", "https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm", NULL);
 
     /* Connect to the pad-added signal */
+    /**
+     * 绑定事件处理函数
+     *
+     * - 当收到pad-added信号时，执行回调连接src_pad和后续元素
+     * - 查看所有支持的所有信号: `gst-inspect-1.0 uridecodebin`
+     */
     g_signal_connect(data.source, "pad-added", G_CALLBACK(pad_added_handler), &data);
 
     /* Start playing */
@@ -87,6 +112,11 @@ int main(int argc, char *argv[])
     }
 
     /* Listen to the bus */
+    /**
+     * 事件循环
+     *  - 收到ERROR和EOS消息终止循环退出程序
+     *  - 收到State_changed消息打印输出
+     */
     bus = gst_element_get_bus(data.pipeline);
     do
     {
@@ -114,6 +144,7 @@ int main(int argc, char *argv[])
                 break;
             case GST_MESSAGE_STATE_CHANGED:
                 /* We are only interested in state-changed messages from the pipeline */
+                /** 仅输出来自管道的消息 */
                 if (GST_MESSAGE_SRC(msg) == GST_OBJECT(data.pipeline))
                 {
                     GstState old_state, new_state, pending_state;
@@ -139,18 +170,25 @@ int main(int argc, char *argv[])
 }
 
 /* This function will be called by the pad-added signal */
+/**
+ * 回调处理函数
+ */
 static void
 pad_added_handler(GstElement *src, GstPad *new_pad, CustomData *data)
 {
+    // 获取目标pad
     GstPad *video_sink_pad = gst_element_get_static_pad(data->videoconvert, "sink");
     GstPad *audio_sink_pad = gst_element_get_static_pad(data->audioconvert, "sink");
     GstPadLinkReturn ret;
     GstCaps *new_pad_caps = NULL;
     GstStructure *new_pad_struct = NULL;
     const gchar *new_pad_type = NULL;
+
+    // 打印输出 源pad名称 和 源element名称
     g_print("Received new pad '%s' from '%s':\n", GST_PAD_NAME(new_pad), GST_ELEMENT_NAME(src));
 
     /* If our converter is already linked, we have nothing to do here */
+    // 检测目标pad是否已经被连接
     if (gst_pad_is_linked(video_sink_pad) && gst_pad_is_linked(audio_sink_pad))
     {
         g_print("We are already linked. Ignoring.\n");
@@ -158,12 +196,17 @@ pad_added_handler(GstElement *src, GstPad *new_pad, CustomData *data)
     }
 
     /* Check the new pad's type */
+    /** 检查当前pad支持的所有capabilities能力（GstCaps[]） */
     new_pad_caps = gst_pad_get_current_caps(new_pad);
+    /** 获取所有capabilities能力的第一个capability（GstCaps结构体） */
     new_pad_struct = gst_caps_get_structure(new_pad_caps, 0);
+    /** 获取capability能力（GstCaps结构体）的name属性 */
     new_pad_type = gst_structure_get_name(new_pad_struct);
+    /** 根据源pad支持的能力名称分别链接后续元素 */
     if (g_str_has_prefix(new_pad_type, "video/x-raw"))
     {
         /* Attempt the link */
+        // 顺序必须是src_pad -> sink_pad
         ret = gst_pad_link(new_pad, video_sink_pad);
         if (GST_PAD_LINK_FAILED(ret))
             g_print("Type is '%s' but link failed.\n", new_pad_type);
@@ -182,7 +225,6 @@ pad_added_handler(GstElement *src, GstPad *new_pad, CustomData *data)
     else
     {
         g_print("It has type '%s' which is not support. Ignoring.\n", new_pad_type);
-        goto exit;
     }
 
 exit:
